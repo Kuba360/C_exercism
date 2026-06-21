4bbbf0f75a0b5af17d9bda1b8cbd74202f04c90e
Jakub <kuba.bartosz.kb@gmail.com>
2026-06-21 20:37:13 +0200

    Optimalization of total funciton

--- grains/grains.c
+++ grains/grains.c
@@ -4,13 +4,9 @@ uint64_t total(void);
 
 
 uint64_t total(void){
-    uint64_t t=0;
-    for(int i=1;i<=64;i++){
-        t+=square(i);
-    }
-    return t;
-
+    return ~0ULL;
 }
+
 uint64_t square(uint8_t index){
     if(index==0){return 0;}
     uint64_t y=1;
