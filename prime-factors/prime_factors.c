0b2b80765d82ce403ab83197f511776e90602860
Kuba <kuba.bartosz.kb@gmail.com>
2026-07-01 10:40:12 +0200

    Solve prime_factors exercise

--- prime-factors/prime_factors.c
+++ prime-factors/prime_factors.c
@@ -1,1 +1,19 @@
 #include "prime_factors.h"
+#include <stdint.h>
+#include <stddef.h>
+
+//#define MAXFACTORS 10
+
+size_t find_factors(uint64_t n, uint64_t factors[static MAXFACTORS]){
+	int p=2;
+	size_t c=0;
+	while(n!=1){
+		if(n%p==0){
+			n/=p;
+			factors[c++]=p;
+		}else{
+			p++;
+		}
+	}
+	return c;
+}
