f7543faf0bf07d3995401aa1784814a7880fdecd
Jakub <kuba.bartosz.kb@gmail.com>
2026-09-04 08:45:42 +0200

    Solve linked_list exercise

--- linked-list/linked_list.c
+++ linked-list/linked_list.c
@@ -1,4 +1,5 @@
 #include "linked_list.h"
+#include <stdlib.h>
 
 struct list_node {
    struct list_node *prev, *next;
@@ -32,7 +33,7 @@ size_t list_count(const struct list *list){
 // inserts item at back of a list
 void list_push(struct list *list, ll_data_t item_data){
    struct list_node *new_n=malloc(sizeof(struct list_node));
-   if(!new_n)return NULL;
+   if(!new_n)return;
    new_n->prev=list->last;
    new_n->data=item_data;
    new_n->next=NULL;
@@ -47,17 +48,91 @@ void list_push(struct list *list, ll_data_t item_data){
 }
 
 // removes item from back of a list
-ll_data_t list_pop(struct list *list);
+ll_data_t list_pop(struct list *list){
+   if(list->last==0)return 0;
+
+   struct list_node * tmp=list->last;
+   ll_data_t x=tmp->data;
+
+   if(list->first==list->last){
+      list->first=NULL;
+      list->last=NULL;
+   }else{
+      list->last=tmp->prev;
+      list->last->next=NULL;
+   }
+   free(tmp);
+   return x;
+}
 
 // inserts item at front of a list
-void list_unshift(struct list *list, ll_data_t item_data);
+void list_unshift(struct list *list, ll_data_t item_data){
+   struct list_node *new_n=malloc(sizeof(struct list_node));
+   if(!new_n)return;
+   new_n->data=item_data;
+   new_n->next=list->first;
+   new_n->prev=NULL;
+
+   if(list->first){
+      list->first->prev=new_n;
+   }else{
+      list->last=new_n;
+   }
+   list->first=new_n;
+}
 
 // removes item from front of a list
-ll_data_t list_shift(struct list *list);
+ll_data_t list_shift(struct list *list){
+   if(list->last==0)return 0;
+
+   struct list_node * tmp=list->first;
+   ll_data_t x=tmp->data;
+
+   if(list->first==list->last){
+      list->first=NULL;
+      list->last=NULL;
+   }else{
+      list->first=tmp->next;
+      list->first->prev=NULL;
+   }
+   free(tmp);
+   return x;
+
+}
 
 // deletes a node that holds the matching data
-void list_delete(struct list *list, ll_data_t data);
+void list_delete(struct list *list, ll_data_t data){
+   struct list_node *current_n=list->first;
+
+   while(current_n!=NULL && current_n->data!=data){
+      current_n=current_n->next;
+   }
+   if(current_n==NULL)return;
+   
+   if(current_n->prev!=NULL){
+      current_n->prev->next=current_n->next;
+   }else{
+      list->first=current_n->next;
+   }
+
+   if(current_n->next!=NULL){
+      current_n->next->prev=current_n->prev;
+   }else{
+      list->last=current_n->prev;
+   }
+
+   free(current_n);
+}
 
 // destroys an entire list
 // list will be a dangling pointer after calling this method on it
-void list_destroy(struct list *list);
+void list_destroy(struct list *list){
+   struct list_node *current_n=list->first;
+
+   while(current_n!=NULL){
+      struct list_node *tmp=current_n->next;
+      free(current_n);
+      current_n=tmp;
+   }
+   free(list);
+}
