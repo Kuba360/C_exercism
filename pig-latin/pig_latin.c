1381c58a350db4be387ddb29e6f5142e53688ce1
Jakub <kuba.bartosz.kb@gmail.com>
2026-09-07 14:19:32 +0200

    Solve pig_latin exercise

--- pig-latin/pig_latin.c
+++ pig-latin/pig_latin.c
@@ -1,6 +1,7 @@
 #include "pig_latin.h"
 #include <string.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 static const char vowels[]={'a','e','i','o','u'};
 
@@ -11,11 +12,11 @@ bool is_vowel(char c){
             vo=true;
             break;
         }
-    return vo;
     }
+    return vo;
 }
 
-char *translate(const char *phrase){
+char *translate_word(const char *phrase){
     int len=(int)strlen(phrase);
     char *new=calloc(len+1+2,sizeof(char));
 
@@ -23,29 +24,79 @@ char *translate(const char *phrase){
 
     if(first_is_vowel || strncmp(phrase,"xr",2)==0 || strncmp(phrase,"yt",2)==0){
         strcpy(new,phrase);
-        new[len]='a';
-        new[len+1]='y';
     }
 
     if(!first_is_vowel){
-        char *p=str(phrase,"qu");
+        char *p=strstr(phrase,"qu");
         int pos_qu=-1;
         if(p!=NULL){
             pos_qu=p-phrase;
-        }
-
-        int num_of_con=1;
-        for(int i=1;i<len;i++){
-            num_of_con++;
-            if(is_vowel(phrase[i])){
-                break;
+            bool qu=true;
+            for(int i=0;i<pos_qu;i++){
+                if(is_vowel(phrase[i])){
+                    qu=false;
+                }
+            }
+            if(qu){
+                int lenght=len-(pos_qu+2);
+                memcpy(new,phrase+pos_qu+2,lenght);
+                memcpy(new+lenght,phrase,pos_qu+2);
             }
         }
-
-
-
+        if(!strlen(new)){
+            int num_of_con=0;
+            int y_pos=-1;
+            for(int i=0;i<len;i++){
+                if(is_vowel(phrase[i])){
+                    break;
+                }
+                if(phrase[i]=='y' && i!=0){
+                    y_pos=i;
+                    printf("Y position is=%d\n",y_pos);
+                    break;
+                }
+                
+                num_of_con++;
+            }
+            int distance=(y_pos==num_of_con)? y_pos: num_of_con;
+            printf("distance: %d\n",distance);
+            strncpy(new,phrase+distance,len-distance);
+            memcpy(new+len-distance,phrase,distance);
+        }
     }
-    
+
+    new[len]='a';
+    new[len+1]='y';
     new[len+2]='\0';
     return new;
+}
+
+
+char *translate(const char *phrase){
+    int len=strlen(phrase);
+    char *result=calloc(len*2+1,sizeof(char));
+
+    int i=0;
+    int out=0;
+    while(i<len){
+        char word[100];
+        int j=0;
+
+        while(phrase[i]!=' ' && phrase[i]!='\0'){
+            word[j++]=phrase[i++];
+        }
+        word[j]='\0';
+        
+        char *translated=translate_word(word);
+        strcpy(result+out,translated);
+        out+=strlen(translated);
+        free(translated);
+        if(phrase[i]==' '){
+            result[out++]=' ';
+            i++;
+        }
+    }
+    result[out]='\0';
+    return result;
+    
 }
--- pig-latin/pig_latin.h
+++ pig-latin/pig_latin.h
@@ -1,6 +1,9 @@
 #ifndef PIG_LATIN_H
 #define PIG_LATIN_H
+#include <stdbool.h>
 
 char *translate(const char *phrase);
+char *translate_word(const char *phrase);
+bool is_vowel(char c);
 
 #endif
